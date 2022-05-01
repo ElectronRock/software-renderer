1a7cdb1d77010bebbe54e827dddb8de1ed6affd6
Gleb Bezborodov <bezborodoff.gleb@gmail.com>
2022-05-01 10:28:11 +0400

    first task : wireframe rendering

--- line.h
+++ line.h
@@ -20,26 +20,7 @@ namespace line {
 
     template<typename TPointSetter>
     void brezenhaim(point p1, point p2, TPointSetter&& setter) {
-        bool steep = false;
-
-        if (std::abs(p1.x - p2.x) < std::abs(p1.y - p2.y)) { // if the line is steep, we transpose the image
-            std::swap(p1.x, p1.y);
-            std::swap(p2.x, p2.y);
-            steep = true;
-        }
-        if (p1.x > p2.x) { // make it left-to-right
-            std::swap(p1, p2);
-        }
-
-        for (auto x{ p1.x }; x <= (p2.x); ++x) {
-            float t = ( x - p1.x) / (float)(p2.x - p1.x);
-            int y = p1.y * (1. - t) + p2.y * t;
-            if (steep) {
-                setter(y, x); // if transposed, de-transpose
-            } else {
-                setter(x, y);
-            }
-        }
+        // TODO :: implement
     }
 
 }
--- main.cpp
+++ main.cpp
@@ -14,11 +14,21 @@ constexpr static unsigned Width { 1280 };
 constexpr static unsigned Height { 720 };
 constexpr static tga_image::color White{ 255, 255, 255, 0 };
 
-int main(){
+int main() {
+    
+    // prepare empty img
     tga_image img(Width, Height);
+    
+    // load desired model
     model in_model("head.obj");
+    
+    // draw wires on the img
     renderer::wireframe(in_model, img, White);
 
+    // TODO:: flip img?
+
+    // store the result
     img.store("out.tga");
+    
     return 0;
 }
